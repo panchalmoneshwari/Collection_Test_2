dd44bcc38456b44c654f57a639c09d5b30443bb7
Moneshwari Panchal <moneshwaripanchal@gmail.com>
2022-06-03 17:27:28 +0530

    commit

---
+++ Collection_Test_2/listmock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Test_2
+{
+    class Listmock
+    {
+        static void Main(string[] args)
+        {
+            List<string> lt = new List<string>();
+            lt.Add("Deepa mam");
+            lt.Add("Manali");
+            lt.Add("Deepa mam");
+            lt.Add("Manali");
+            lt.Add("Deepa mam");
+            lt.Add("Deepa mam");
+            Dictionary<string, int> ds = new Dictionary<string, int>();
+            
+            
+            foreach (dynamic d in lt)
+            {
+                
+                if(lt[0]==)
+            }
+            Console.ReadLine();
+        }
+    }
+}
